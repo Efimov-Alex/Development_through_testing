6d8841508928b919540e5ac7a45c12b7f5870b39
Efimov-Alex <fastic2018l@yandex.ru>
2023-09-01 11:26:53 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> phones = new HashMap<>();
+
+    public int add(String name, String number){
+        return 0;
+    }
+
+    public Map<String, String> getPhones() {
+        return phones;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class PhoneBookTest {
+    @Test
+    public void testAddFirst(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Петя", "+79456739878");
+        phoneBook.add("Вика", "+79153856723");
+
+        assertThat(phoneBook.getPhones().size(), equalTo(2));
+    }
+
+    @Test
+    public void testAddSecond(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Петя", "+79456739878");
+        phoneBook.add("Вика", "+79153856723");
+        phoneBook.add("Никита", "+79153856723");
+
+        assertThat(phoneBook.getPhones().size(), equalTo(3));
+    }
 }
